059053218cfdd6314e8ecd17ba7a3b3904e58cbe
kradunches <cheryaev@yandex.ru>
2025-01-06 00:26:02 +0300

    added transactions

--- src/hashable.rs
+++ src/hashable.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 pub trait Hashable {
-    fn bytes(&self) -> Vec<u8>;
+    fn bytes(&self) -> Hash;
 
     fn hash(&self) -> Vec<u8> {
         crypto_hash::digest(crypto_hash::Algorithm::SHA256, &self.bytes())
--- src/tests.rs
+++ src/tests.rs
@@ -1,55 +1,135 @@
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{ now, Block, Blockchain };
-
-    fn create_test_blockchain() -> Blockchain {
+    use crate::{ now, transaction, Block, Blockchain, Transaction };
+    #[test]
+    fn invalid_input() {
         let difficulty = 0x000fffffffffffffffffffffffffffff;
-        let mut block = Block::new(0, now(), vec![0; 32], 0, "Genesis block!".to_owned(), difficulty);
+        //First transaction (genesis)
+        let mut genesis_block = Block::new(0, now(), vec![0; 32], vec![
+            Transaction{
+                inputs: vec![ ],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Alice".to_owned(),
+                        value: 50,
+                    },transaction::Output {
+                        to_addr: "Bob".to_owned(),
+                        value: 7,
+                    }
+                ]
+        }], difficulty);
+    
+        genesis_block.mine();
+        println!("Mined genesids block {:?}", &genesis_block);
+    
+        let mut last_hash = genesis_block.hash.clone();
+    
+        let mut blockchain = Blockchain::new();
+        
+        blockchain.update_with_block(genesis_block).expect("Failed to add genesis block");
+    
+        // Second transaction
+        let mut block = Block::new(1, now(), last_hash, vec![
+            Transaction {
+                inputs: vec![ ],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Chris".to_owned(),
+                        value: 536,
+                    },
+                ],
+            },
+            Transaction {
+                inputs: vec![
+                    blockchain.blocks[0].transactions[0].outputs[0].clone(),
+                    transaction::Output {
+                        to_addr: "Bodfjkldfj".to_owned(),
+                        value: 3432424,
+                    },
+                ],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Alice".to_owned(),
+                        value: 34,
+                    },
+                    transaction::Output {
+                        to_addr: "Bob".to_owned(),
+                        value: 12,
+                    },
+                ],
+            },
+        ], difficulty);
+    
         block.mine();
-        let mut last_hash = block.hash.clone();
-        let mut blockchain = Blockchain {
-            blocks: vec![block],
-        };
-        for i in 1..=10 {
-            let mut block = Block::new(i, now(), last_hash, 0, "Another block".to_owned(), difficulty);
     
-            block.mine();
+        println!("Mined block {:?}", &block);
     
-            last_hash = block.hash.clone();
+        last_hash = block.hash.clone();
     
-            blockchain.blocks.push(block);
-        }
-        blockchain
+        assert!(blockchain.update_with_block(block).is_err());
     }
     #[test]
-    fn index_verification() {
-        let mut blockchain = create_test_blockchain();
-        blockchain.blocks[3].index = 4;
-        assert!(!&blockchain.verify());
-    }
-    #[test]
-    fn difficulty_verification() {
-        let mut blockchain = create_test_blockchain();
-        blockchain.blocks[3].payload = "No".to_owned();
-        assert!(!&blockchain.verify());
-    }
-    #[test]
-    fn time_verification() {
-        let mut blockchain = create_test_blockchain();
-        blockchain.blocks[3].timestamp = 0;
-        assert!(!&blockchain.verify());
-    }
-    #[test]
-    fn hash_verification() {
-        let mut blockchain = create_test_blockchain();
-        blockchain.blocks[3].hash[8] += 1;
-        assert!(!&blockchain.verify());
-    }
-    #[test]
-    fn genesis_hash_verification() {
-        let mut blockchain = create_test_blockchain();
-        blockchain.blocks[0].hash[1] += 1;
-        assert!(!&blockchain.verify());
+    fn spending_more_than_you_have() {
+        let difficulty = 0x000fffffffffffffffffffffffffffff;
+        //First transaction (genesis)
+        let mut genesis_block = Block::new(0, now(), vec![0; 32], vec![
+            Transaction{
+                inputs: vec![ ],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Alice".to_owned(),
+                        value: 50,
+                    },transaction::Output {
+                        to_addr: "Bob".to_owned(),
+                        value: 7,
+                    }
+                ]
+        }], difficulty);
+    
+        genesis_block.mine();
+        println!("Mined genesids block {:?}", &genesis_block);
+    
+        let mut last_hash = genesis_block.hash.clone();
+    
+        let mut blockchain = Blockchain::new();
+        
+        blockchain.update_with_block(genesis_block).expect("Failed to add genesis block");
+    
+        // Second transaction
+        let mut block = Block::new(1, now(), last_hash, vec![
+            Transaction {
+                inputs: vec![ ],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Chris".to_owned(),
+                        value: 536,
+                    },
+                ],
+            },
+            Transaction {
+                inputs: vec![
+                    blockchain.blocks[0].transactions[0].outputs[0].clone(),
+                ],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Alice".to_owned(),
+                        value: 34443,
+                    },
+                    transaction::Output {
+                        to_addr: "Bob".to_owned(),
+                        value: 12234,
+                    },
+                ],
+            },
+        ], difficulty);
+    
+        block.mine();
+    
+        println!("Mined block {:?}", &block);
+    
+        last_hash = block.hash.clone();
+    
+        assert!(blockchain.update_with_block(block).is_err());
     }
 }
